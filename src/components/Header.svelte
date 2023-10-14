872aa82492a885ed93cc38f84f87caf5168bc928
Jose Von Chong <javch1995@gmail.com>
2023-10-13 19:30:41 -0500

    header updated

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,7 +2,8 @@
     // You can add JavaScript logic here if needed
   </script>
   
-  <header class=" w-full bg-slate-900 p-8 flex flex-row items-center justify-between ">
+  <header class=" w-full bg-slate-900 p-8    ">
+    <div class="container flex items-center justify-between">
     <h1 class="text-2xl font-bold">Welcome to My Website</h1>
     <nav class="mt-2 flex">
         <ul class="list-none flex space-x-4 ml-auto">
@@ -11,6 +12,7 @@
           <li><a href="#" class="text-white">Contact</a></li>
         </ul>
       </nav>
+    </div>
   </header>
   
 
