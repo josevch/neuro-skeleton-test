9059bfaad8676325224d895f3f36378fbd2a7611
Jose Von Chong <javch1995@gmail.com>
2023-10-16 23:07:05 -0500

    Slider init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,13 @@
 	import Beneficios from "../components/Beneficios.svelte";
 import Header from "../components/Header.svelte";
 	import Pillards from "../components/Pillards.svelte";
+	import Slider from "../components/Slider.svelte";
 	import Twoside from "../components/Twoside.svelte";
 
 </script>
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 <Header></Header>
+<Slider></Slider>
 <Pillards></Pillards>
 <Beneficios></Beneficios>
 <Twoside></Twoside>
